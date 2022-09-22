21e2ac23478ef29eecf0e459c5d77b343294d061
Leonardo <ljcl79@gmail.com>
2022-09-21 22:48:55 -0300

    Aula 2

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -3,29 +3,44 @@ using ByteBank.DatosCliente;
 
 static void MetodoInicial(int divisor)
 {
-    TestDivision(divisor);
-    
+    try
+    {
+        TestDivision(divisor);
+    }
+    catch (NullReferenceException)
+    {
+        Console.WriteLine("Referencia nula");
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("No es posible ejecutar una división entre 0");
+    }
+
+
 }
 
 static void TestDivision(int divisor)
 {
     //Puede suceder una excepción si el divisor es 0.
     int resultado = 0;
-    try
-    {
-        resultado = Dividir(10, divisor);
-        Console.WriteLine("El resultado de dividir 10 entre " + divisor + " es: " + resultado);
-    } catch (DivideByZeroException)
-    {
-        Console.WriteLine("No es posible ejecutar una división entre 0");
-    }
-    
+
+    resultado = Dividir(10, divisor);
+    Console.WriteLine("El resultado de dividir 10 entre " + divisor + " es: " + resultado);
+        
 }
 
 static int Dividir(int numero, int divisor)
 {
     
-    return numero / divisor;
+    try
+    {
+        return numero / divisor;
+    } catch
+    {
+        Console.WriteLine("Excepción ejecutando división de " + numero + " entre " + divisor);
+        throw;
+    }
+    
 }
 
 
